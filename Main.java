a8afc828915da3e4b9c183d70e1a38bdfd9464a3
Mohsen-ameli <mohsen.ameli83@gmail.com>
2022-04-28 19:20:18 -0400

    finished BR and Main java files i guess

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        BattleRoyal rankedBR = new BattleRoyal("Snowy", "Sweden", 10, 69, true);
+
+        System.out.println(rankedBR.toString());
+    }
+}
