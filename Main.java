a7e00bd5c780e611ca54448f5aabe5b08c065322
Mohsen-ameli <mohsen.ameli83@gmail.com>
2022-04-29 09:10:48 -0400

    main arena

--- Main.java
+++ Main.java
@@ -1,7 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         BattleRoyal rankedBR = new BattleRoyal("Snowy", "Sweden", 10, 69, true);
+        Arena rankedArena = new Arena(7, 5, 3, true);
 
         System.out.println(rankedBR.toString());
+        System.out.println();
+        System.out.println(rankedArena.toString());
     }
 }
