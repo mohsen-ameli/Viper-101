048c56d081a2877b690208cac30307ca97281800
Arshia-a <arshiavixon@gmail.com>
2022-04-29 09:05:45 -0400

    arena added

---
+++ Arena.java
@@ -0,0 +1,42 @@
+public class Arena {
+        /*
+    Attributes
+    */
+    private int round;
+    private double time;
+    private int numPlayers;
+    private boolean ranked;
+
+    /*
+    Constructor
+    */
+    public Arena(int round, double time, int numPlayers, Boolean ranked) {
+
+        this.round   = round;
+        this.time    = time;
+        this.numPlayers = numPlayers;
+        this.ranked     = ranked;
+    }
+
+    /*
+    Accessors
+    */
+    public int getRound() {return this.round;}
+    public double getTime() {return this.time;}
+    public int getNumPlayers() {return this.numPlayers;}
+    public boolean getRanked() {return this.ranked;}
+
+    /*
+    Methods
+    */
+    public String toString() {
+        String builder = "";
+
+        builder += "Time : " + this.time + ", ";
+        builder += "Round : " + this.round + ", ";
+        builder += "Number of Players : " + this.numPlayers + ", ";
+        builder += "Ranked : " + this.ranked;
+
+        return builder;
+    }
+}
