4f336f18327b2c7e4a5b833a2f2ca7c258d28df2
Themistzan12 <100785425+Themistzan12@users.noreply.github.com>
2022-05-03 23:01:23 -0400

    Changes to the shop

--- Code/Shop.java
+++ Code/Shop.java
@@ -18,7 +18,7 @@ public class Shop {
     Constructor
     */
   
-    public void Gun(String characterSkins, int currency, String gunSkins, String bundle, String charms) {
+    public void Shop (String characterSkins, int currency, String gunSkins, String bundle, String charms) {
         this.characterSkins = characterSkins;
         this.currency = currency;
         this.gunSkins = gunSkins;
@@ -38,14 +38,44 @@ public class Shop {
     /*
     Methods
     */
+    
+    public void buyCurr () { 
+        if (this.currency < 0 && this.currency + 1000) { 
+            this.currency = this.currency + 1000;
+            System.out.println("You cannot buy any thing sorry");
+            System.out.printf("You can buy anything in the store", this.currency);
+        }
+    }
+    public void buySkin () { 
+        if (this.characterSkins < 0 && this.characterSkins + 75) { 
+            this.characterSkins = this.characterSkins + 75;
+            System.out.println("This skin is free");
+            System.out.printf("You can buy any single skin you want", this.characterSkins);
+
+    }
+    public void buyBoundle () { 
+        if (this.bundle < 0 && this.bundle + 150) { 
+            this.bundle = this.bundle + 150;
+            System.out.println("This boundle is free");
+            System.out.printf("You can buy any single boundle", this.characterSkins);
+    
+
+    }
+    public void buygunskins () { 
+        if (this.gunSkins < 0 && this.gunSkins + 45 ) { 
+            this.gunSkins = this.gunSkins + 45;
+            System.out.println("This gun skin is free");
+            System.out.printf("Yu can buy any single gun skin", this.gunSkins); 
+    
+    
     public String toString() {
         String builder = "";
 
-        builder += "Weapon name : " + this.characterSkins + ", ";
-        builder += "Weapon ammo : " + this.currency + ", ";
-        builder += "Weapon damage : " + this.gunSkins + ", ";
-        builder += "Weapon recoil : " + this.bundle + ", ";
-        builder += "accuracy : " + this.charms;
+        builder += "characterskins : " + this.characterSkins + ", ";
+        builder += "currency : " + this.currency + ", ";
+        builder += "gunskins : " + this.gunSkins + ", ";
+        builder += "bundlee : " + this.bundle + ", ";
+        builder += "charms : " + this.charms;
 
         return builder;
     }
---
+++ Shop.java
@@ -0,0 +1,52 @@
+
+ * Course: ICS4U1
+ * Date: Apr. 28th, 2022
+ * @author Zain Siddiqui
+ */
+
+public class Shop {
+   /*
+    Attributes
+    */
+    private String CharacterSkins;
+    private int currency;
+    private String GunSkins;
+    private String Bundle;
+    private String Charms;
+   
+   /*
+    Constructor
+    */
+  
+    public Shop (String CharacterSkins, int currency, String GunSkins, String Bundle, String Charms ) {
+        this.CharacterSkins = CharacterSkins;
+        this.currency = currency;
+        this.GunSkins = GunSkins;
+        this.Bundle = Bundle;
+        this.Charms = Charms; 
+    }
+    /*
+    Accessors
+    */
+
+    public String getCharacterSkins() {return characterskins;}
+    public int getcurrency() {return currency;}
+    public int getGunSkins() {return sunskins;}
+    public String getBundle() {return Bundle;}
+    public int getCharms() {return Charms;}
+    
+    /*
+    Methods
+    */
+    public String toString() {
+        String builder = "";
+
+        builder += "Weapon name : " + this.CharacterSkins + ", ";
+        builder += "Weapon ammo : " + this.currency + ", ";
+        builder += "Weapon damage : " + this.GunSkins + ", ";
+        builder += "Weapon recoil : " + this.Bundle + ", ";
+        builder += "accuracy : " + this.Charms;
+
+        return builder;
+    }
+}
